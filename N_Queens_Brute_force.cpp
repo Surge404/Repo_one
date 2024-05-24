a4cd0f97ec3d074ec1fcce7078612de61bf33c9b
Surge404 <165484801+Surge404@users.noreply.github.com>
2024-05-24 13:23:15 +0530

    Update N_Queens_Brute_force.cpp

--- N_Queens_Brute_force.cpp
+++ N_Queens_Brute_force.cpp
@@ -1,58 +1,49 @@
 class Solution {
-    bool safe_hai_kya(vector<string> &board, int row, int col, int n){
-        cout<<"Is safe called\n";
-        //horizontal attacks from some queen in left
-        for(int i = 0;i<=col;i++){
-            if(board[row][i]=='Q'){cout<<"left\n";return false;}
+bool safety_check_kar(int row, int col, vector<string> square, int n){
+    // left
+    for(int i = 0;i<col;i++){
+        if(square[row][i]=='Q'){
+            return false;
         }
-        //atacks from diagonally upwards queens from left side
-        int i = row, j = col;
-        while(i>=0&&j>=0){
-        if(board[i][j]=='Q'){cout<<"diagonal, up\n";return false;}    
+    }
+    //up diagonal left
+    int i = row, j = col;
+    while(i>=0&&j>=0){
+        if(square[i][j]=='Q'){
+            return false;
+        }
         i--;j--;
-
-        }
-
-
-
-        //atacks from diagonally downwards queens from left side
-        i = row, j = col;
-        while(i<n&&j>=0){
-        if(board[i][j]=='Q'){cout<<"diagonal down\n";return false;}    
-        i++;j--;
-
-        }
-        return true;
     }
 
-
-
-
-    void solve_karo(int col, vector<string> &board, vector<vector<string>> & answer, int n){
-        if(col==n){cout<<board[0]<<endl<<board[1]<<endl<<board[2]<<endl<<board[3]<<endl;
-            answer.push_back(board);
-
+    i = row, j = col;
+    while(i<n&&j>=0){
+        if(square[i][j]=='Q'){
+            return false;
         }
-        cout<<"Hello";
-        for(int i=0;i<n;i++){
-            if(safe_hai_kya(board,i,col,n)){
-                board[i][col]='Q';
-                cout<<"Yo col is "<<col<<endl;
-                solve_karo(col+1,board,answer,n);
-                board[i][col]='.';    
+        i++;j--;
+    
+    }
+    return true;
+}
+
+    void solve_kar_de_bhaai_mere(int col, vector<vector<string>> & ans, vector<string> square, int n){
+        if(col>=n){ans.push_back(square);
+        return;}
+        for(int i = 0;i<n;i++){
+            if(safety_check_kar(i, col, square, n))
+            {square[i][col]='Q';
+            solve_kar_de_bhaai_mere(col+1, ans, square,n);
+            square[i][col]='.';
 
             }
-            
-
         }
     }
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> answer;
-        vector<string> board(n,string(n,'.'));
-        solve_karo(0,board,answer, n);
-        
-        return answer;
 
+        vector<vector<string>> ans;
+        vector<string> square(n, string(n, '.'));
+        solve_kar_de_bhaai_mere(0,ans,square,n);
+        return ans;
     }
 };
